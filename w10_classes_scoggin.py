b1413a911fd62c43fb03336fe027f291a8520aa7
shawn901 <106951266+shawn901@users.noreply.github.com>
2022-06-06 00:51:44 -0600

    Add files via upload

---
+++ w10_classes_scoggin.py
@@ -0,0 +1,70 @@
+"""
+Name: Shawn Scoggin
+Course: ICT 4370-1
+Date: 6/05/2022
+Week 10 - Stock Earnings with GUI and Yahoo Finance API
+This program will create a report calculating investor stock earnings or losses
+"""
+
+#import classes
+from datetime import date, datetime, timedelta
+import datetime
+import yfinance as yahooFinance
+from pandas.tseries.offsets import BDay
+
+#Create Class for Stocks
+class StockShares:
+    def __init__(self, purchaseID, stock_symbol, number_shares, purchase_date):
+        self.purchaseID = purchaseID
+        self.stock_symbol = stock_symbol
+        self.number_shares = number_shares
+        self.purchase_date = purchase_date
+
+    def yahooPurchasePrice(self):
+        startDate = datetime.datetime.strptime(self.purchase_date, '%Y-%m-%d')
+        endDate = startDate + timedelta(days=1)
+        self.yahooStockInfo = yahooFinance.Ticker(self.stock_symbol)
+        purchaseDatePrice = self.yahooStockInfo.history(start=startDate, end=endDate)
+        self.purchaseDatePrice = purchaseDatePrice['Close'].iloc[-1]
+        return self.purchaseDatePrice
+    
+    #Use Pandas BDay method to find previous business day, which will be used to view current stock price
+    def businessDate(self):
+        self.lastBusinessDay = (datetime.datetime.today() - BDay(1))
+        lastBusinessDaytoString = self.lastBusinessDay.strftime('%Y-%m-%d')
+        return lastBusinessDaytoString
+
+    def businessDatePrice(self):
+        endDate2 = self.lastBusinessDay + timedelta(days=1)
+        businessDayPrice = self.yahooStockInfo.history(start=self.lastBusinessDay, end=endDate2)
+        self.businessDayPrice = businessDayPrice['Close'].iloc[-1]
+        return self.businessDayPrice
+
+    #define function to calculate the total investment returns (gains or losses) as dollar amount
+    def calcInvestmentReturn(self):
+        #subtract current stock value and purchase value, multiply by stocks purchased
+        try:
+            investmentResult = (float(self.businessDayPrice) - float(self.purchaseDatePrice)) * self.number_shares
+        except:
+            print("String type variables provided, when expecting number")
+
+        #format the variable with 2 floating decimal places
+        investmentResult = "{0:.2f}".format(investmentResult)
+    
+        #return the total investment returns as dollar amount
+        return investmentResult
+
+#Create Class for Investors
+class Investor:
+    def __init__(self, first_name, last_name, investorID, address, phone_number):
+        """Initialize Investor attributes."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.investorID = investorID
+        self.address = address
+        self.phone_number = phone_number
+    
+    #print Investor info
+    def investor_info(self):
+        print(f" Investment report for {self.first_name} {self.last_name}, Investor ID = {self.investorID}")
+
---
+++ w10_main_scoggin.py
@@ -0,0 +1,123 @@
+"""
+Name: Shawn Scoggin
+Course: ICT 4370-1
+Date: 6/05/2022
+Week 10 - Stock Earnings with GUI and Yahoo Finance API
+This program will create a report calculating investor stock earnings or losses
+"""
+
+#import classes
+from ctypes import alignment
+from tkinter import *
+
+#import classes from file
+from w10_classes_scoggin import *
+
+#Instantiate investor objects
+bob_smith = Investor('Bob','Smith','001', '123 Main St, Denver, CO, 80001', '303-485-9540')
+#sally_jones = Investor('Sally','Jones','002', '456 Front St, Denver, CO, 80002', '720-835-1722')
+
+#Reference external file for Bob's stocks
+bobs_stocks_file = 'c:\Temp\w10_Bobs_Stocks.csv'
+
+#Read external file for Bob's stocks
+#Error handling for reading the stocks file
+try:
+    with open(bobs_stocks_file) as file_object:
+        header = (file_object.readline()).rstrip()
+        contents = file_object.readlines()
+except FileNotFoundError:
+    print(f"Sorry, the file {bobs_stocks_file} does not exist.")
+    exit()
+
+#Create index variables for the columns for Bob's stocks
+header_split = header.split(',')
+symbol_index = header_split.index('SYMBOL')
+shares_index = header_split.index('NO_SHARES')
+purchase_date_index = header_split.index('PURCHASE_DATE')
+
+#Create list of Bob's stocks
+bobs_stocks = []
+stock_index = 1
+
+#Loop through the external file lines and instantiate objects for Bob's stocks
+for line in contents:
+  line_split = line.split(',')
+  stock_purchase = StockShares(stock_index, line_split[symbol_index], int(line_split[shares_index]), (line_split[purchase_date_index]).rstrip())
+  bobs_stocks.append(stock_purchase)
+  stock_index = stock_index + 1
+
+#Define GUI window with tkinter
+window = Tk()
+window.title("Welcome to Investment, Inc")
+window.geometry('800x600')
+
+#Define a login message for the financial consultant
+welcome_lbl = Label(window, text="**You are logged in as Joe Banker**", font=12, pady=10, padx=150)
+welcome_lbl.grid(columnspan=7, row=0)
+
+#Define a prompt for the financial consultant
+info_lbl = Label(window, text="Select the investor profile to load")
+info_lbl.grid(columnspan=2, row=1)
+
+#Define an informational message
+choice_lbl = Label(window, text="[pending investor choice]")
+choice_lbl.grid(columnspan=7, row=2)
+
+#Define method for button actions
+def click_btn1():
+    choice_lbl.configure(text="Bob Smith was selected", font=12)
+    output_column = 0
+    output_row = 4
+    #loop Bob's Stocks print report 
+    tableHeaders = ["Stock Symbol","# of Shares","Purchase Date","Purchase Price","Latest Date","Latest Price","Gains/Loss"]
+    tableHeaderCount = 0
+    #Print the headers in window
+    for tableHeader in tableHeaders:
+        Label(window,text=tableHeader).grid(column=tableHeaderCount, row=3, sticky=W)
+        tableHeaderCount = tableHeaderCount + 1
+    #Loop through all of Bob's stocks
+    for stock in bobs_stocks:
+        output_column = 0
+
+        #Call yahooPurchasePrice() method to obtain the price of the stock at time of purchase
+        purchase_price = float("{0:.2f}".format(stock.yahooPurchasePrice()))
+        
+        #Call businessDate() method to get the previous Business day which will contain stock data
+        previousBusinessDay = stock.businessDate()
+        
+        #Call method to get previous Business day closing price
+        recentPrice = stock.businessDatePrice()
+        recentPrice = float("{0:.2f}".format(recentPrice))
+        
+        #Call method to get total gains/loss
+        gains_loss = stock.calcInvestmentReturn()
+
+        #Define information about the stock
+        stock_info = stock.stock_symbol, stock.number_shares, stock.purchase_date, purchase_price, previousBusinessDay, recentPrice, gains_loss
+        
+        #Loop through the stock information to print to window
+        for info in stock_info:
+            Label(window, text=info).grid(column=output_column, row=output_row, sticky=W)
+            output_column = output_column + 1
+        
+        #Move to the next row to print the next stock
+        output_row = output_row + 1
+
+'''
+def click_btn2():
+    choice_lbl.configure(text="Sally Jones was selected")
+'''
+
+#Define button for investor Bob Smith
+investor_btn1 = Button(window, text="Bob Smith", command=click_btn1)
+investor_btn1.grid(column=2, row=1)
+
+'''
+#Define button for invester Sally Jones
+investor_btn2 = Button(window, text="Sally Jones", command=click_btn2)
+investor_btn2.grid(column=3, row=1)
+'''
+
+
+window.mainloop()
